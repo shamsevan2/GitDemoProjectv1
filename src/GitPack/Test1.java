6e7ecf8f2fdde400acff7ceed1644b7684dca6c3
EVAN <EVAN@192.168.0.26>
2022-12-26 16:01:04 -0500

    Hi Lead, i have updated the Test1 class condition sections.

--- src/GitPack/Test1.java
+++ src/GitPack/Test1.java
@@ -5,7 +5,8 @@ public class Test1 {
 	public static void main(String[] args) {
 		String hungry="yes";
 		
-		if(hungry=="yes") {
+		if(hungry=="yes" || hungry=="y" || hungry=="Y" ) {
+				
 			System.out.println("eat something");
 		}
 		else {
